2e87f20eb0f4f089973e361641a28e471e09ec1c
Zdeněk Jašek <zdenek.jasek@gmail.com>
2015-06-19 20:10:15 +0200

    Statusove diody pro polozky shopu

--- cookieClicker.ino
+++ cookieClicker.ino
@@ -24,10 +24,10 @@ byte shopClonerBtn = 18;
 byte currentStateShopClonerBtn; 
 byte lastStateShopClonerBtn = 0;
 
-unsigned long cookiesQuantity =  0;  // celkový počet cookies
-long increment = 1;                  // přičítaná hodnota
-int priceOfMultiplier[] = {10,20,30,40,50,250,600,9999999};          // cena násobiče cookies
-byte counterMultiplierBuy = 0;       // kolikrát koupeno
+unsigned long cookiesQuantity =  0;                              // celkový počet cookies
+long increment = 1;                                              // přičítaná hodnota
+int priceOfMultiplier[] = {10,20,30,40,50,250,600,9999999};      // cena násobiče cookies
+byte counterMultiplierBuy = 0;                                   // kolikrát koupeno
 
 long priceFarm = 100;
 byte percentageRaiseFarm = 10;
@@ -45,6 +45,20 @@ long priceCloner = 10000;
 byte percentageRaiseCloner = 50;
 int addCloner = 100;
 
+byte diodeMultiplierG = 42;
+byte diodeMultiplierR = 43;
+
+byte diodeFarmG = 44;
+byte diodeFarmR = 45;
+
+byte diodeFactoryG = 46;
+byte diodeFactoryR = 47;
+
+byte diodeMineG = 48;
+byte diodeMineR = 49;
+
+byte diodeClonerG = 50;
+byte diodeClonerR = 51;
 
 String infoMsg;
 unsigned long msgDelay = 0;
@@ -61,6 +75,26 @@ void setup() {
   pinMode(shopFactoryBtn, INPUT);   
   pinMode(shopMineBtn, INPUT);   
   pinMode(shopClonerBtn, INPUT);   
+  
+  pinMode(diodeMultiplierG, OUTPUT);
+  pinMode(diodeMultiplierR, OUTPUT);
+  pinMode(diodeFarmG, OUTPUT);
+  pinMode(diodeFarmR, OUTPUT);
+  pinMode(diodeFactoryG, OUTPUT);
+  pinMode(diodeFactoryR, OUTPUT);
+  pinMode(diodeMineG, OUTPUT);
+  pinMode(diodeMineR, OUTPUT);
+  pinMode(diodeClonerG, OUTPUT);
+  pinMode(diodeClonerR, OUTPUT);  
+  
+  digitalWrite(diodeMultiplierG, 1);
+  digitalWrite(diodeMultiplierR, 0);
+  digitalWrite(diodeFarmG, 1);
+  digitalWrite(diodeFarmR, 0);
+  digitalWrite(diodeFactoryG, 1);
+  digitalWrite(diodeFactoryR, 0);
+  digitalWrite(diodeMineG, 1);
+  digitalWrite(diodeMineR, 0);
   lastSecond = millis();
   infoMsg = String("Cookies");
 }
@@ -68,7 +102,7 @@ void setup() {
 boolean enoughCookies(unsigned long price){
   if (price > cookiesQuantity)  
   {
-    infoMsg = String("Malo cookies");
+//    infoMsg = String("Malo cookies");
     msgDelay = millis() + 2000;
     return false;
   }
@@ -78,13 +112,73 @@ boolean enoughCookies(unsigned long price){
   }
 }
 
+void shopStatus() {
+  if (enoughCookies(priceOfMultiplier[counterMultiplierBuy]))
+  {
+    digitalWrite(diodeMultiplierG, 1);
+    digitalWrite(diodeMultiplierR, 0);
+  }
+  else
+  {
+    digitalWrite(diodeMultiplierG, 0);
+    digitalWrite(diodeMultiplierR, 1);
+  }
+  
+  
+  if (enoughCookies(priceFarm))
+  {
+    digitalWrite(diodeFarmG, 1);
+    digitalWrite(diodeFarmR, 0);
+  }
+  else
+  {
+    digitalWrite(diodeFarmG, 0);
+    digitalWrite(diodeFarmR, 1);
+  }
+  
+  
+  if (enoughCookies(priceFactory))
+  {
+    digitalWrite(diodeFactoryG, 1);
+    digitalWrite(diodeFactoryR, 0);
+  }
+  else
+  {
+    digitalWrite(diodeFactoryG, 0);
+    digitalWrite(diodeFactoryR, 1);
+  }  
+  
+  if (enoughCookies(priceMine))
+  {
+    digitalWrite(diodeMineG, 1);
+    digitalWrite(diodeMineR, 0);
+  }
+  else
+  {
+    digitalWrite(diodeMineG, 0);
+    digitalWrite(diodeMineR, 1);
+  }
+  
+  if (enoughCookies(priceCloner))
+  {
+    digitalWrite(diodeClonerG, 1);
+    digitalWrite(diodeClonerR, 0);
+  }
+  else
+  {
+    digitalWrite(diodeClonerG, 0);
+    digitalWrite(diodeClonerR, 1);
+  }  
+  
+}
+
 long shopEngine(long price, byte raise, int add){
   long newPrice;
 	if (enoughCookies(price))  
 	{
 	  perSecondAdd += add;
           newPrice = price + (price*raise/100);
-          infoMsg = String(newPrice);
+//          infoMsg = String(newPrice);
           msgDelay = millis() + 2000;
           cookiesQuantity -= price;
 	  return newPrice;
@@ -105,16 +199,20 @@ void loop() {
   currentStateShopMineBtn = digitalRead(shopMineBtn);
   currentStateShopClonerBtn = digitalRead(shopClonerBtn);
   
-  
-  if(currentStateCookieBtn == 1 && lastStateCookieBtn == 0){
+  shopStatus();
+    
+  if(currentStateCookieBtn == 1 && lastStateCookieBtn == 0)
+  {
     cookiesQuantity += increment;
     lastStateCookieBtn = 1;
   }
-  else if (currentStateCookieBtn == 0 && lastStateCookieBtn == 1)     lastStateCookieBtn = 0;
+  else if (currentStateCookieBtn == 0 && lastStateCookieBtn == 1) lastStateCookieBtn = 0;
   
-  if(currentStateMultiplierBtn == 1 && lastStateMultiplierBtn == 0){  
+  if(currentStateMultiplierBtn == 1 && lastStateMultiplierBtn == 0)
+  {  
     enoughCookies(priceOfMultiplier[counterMultiplierBuy]);
-    if(cookiesQuantity >= priceOfMultiplier[counterMultiplierBuy]){
+    if(cookiesQuantity >= priceOfMultiplier[counterMultiplierBuy])
+    {
       increment *= 2;
       cookiesQuantity -= priceOfMultiplier[counterMultiplierBuy];         
       counterMultiplierBuy++;
@@ -125,7 +223,8 @@ void loop() {
   
   // Tlačítko 1------------------------------------------------------------------------------------
   
-  if(currentStateShopFarmBtn == 1 && lastStateShopFarmBtn == 0){   
+  if(currentStateShopFarmBtn == 1 && lastStateShopFarmBtn == 0)
+  {   
       priceFarm = shopEngine(priceFarm, percentageRaiseFarm, addFarm); 
       lastStateShopFarmBtn = 1;
   }
@@ -133,7 +232,8 @@ void loop() {
   
   // Tlačítko 2------------------------------------------------------------------------------------
   
-  if(currentStateShopFactoryBtn == 1 && lastStateShopFactoryBtn == 0){  
+  if(currentStateShopFactoryBtn == 1 && lastStateShopFactoryBtn == 0)
+  {  
     priceFactory = shopEngine(priceFactory, percentageRaiseFactory, addFactory); 
     lastStateShopFactoryBtn = 1;
   }
@@ -141,7 +241,8 @@ void loop() {
 
   // Tlačítko 3------------------------------------------------------------------------------------
   
-  if(currentStateShopMineBtn == 1 && lastStateShopMineBtn == 0){  
+  if(currentStateShopMineBtn == 1 && lastStateShopMineBtn == 0)
+  {  
     priceMine = shopEngine(priceMine, percentageRaiseMine, addMine); 
     lastStateShopMineBtn = 1;
   }
@@ -149,7 +250,8 @@ void loop() {
   
     // Tlačítko 4------------------------------------------------------------------------------------
   
-  if(currentStateShopClonerBtn == 1 && lastStateShopClonerBtn == 0){  
+  if(currentStateShopClonerBtn == 1 && lastStateShopClonerBtn == 0)
+  {  
     priceCloner = shopEngine(priceCloner, percentageRaiseCloner, addCloner); 
     lastStateShopClonerBtn = 1;
   }
@@ -158,16 +260,19 @@ void loop() {
 
   
 
-if((millis() - lastSecond) > 1000) {
-
-lastSecond = millis();
-cookiesQuantity += perSecondAdd;
+if((millis() - lastSecond) > 1000) 
+{
+  lastSecond = millis();
+  cookiesQuantity += perSecondAdd;
 }
   
   delay(30);
   lcd.clear();     
   lcd.setCursor(0, 0);
-  if (msgDelay < millis() ) {    infoMsg = String("Cookies");    }
+  if (msgDelay < millis())
+  {
+    infoMsg = String("Cookies");    
+  }
   lcd.print(infoMsg);
   lcd.setCursor(0, 1);
   lcd.print(cookiesQuantity);
